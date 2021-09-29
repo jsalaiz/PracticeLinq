5f8fb18be2fff2955de3db2af0dcd28598e91c05
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-28 20:49:18 -0400

    Completed the exercise

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGameList = new List<string>()
+            {
+                "Zelda",
+                "Minecraft",
+                "Terrafria",
+                "Skyrim",
+                "Bioshock"
+            };
+
+            var videoGameListByLength = videoGameList.OrderBy(x => x.Length);
+            Console.WriteLine(string.Join(", ", videoGameListByLength));
+        }
+    }
+}
